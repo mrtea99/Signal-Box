cc5b4f3fad78d79ecb5a68b6fde53caf3a4f2400
Stephen Train <steve.train@gmail.com>
2020-09-22 10:57:23 +0100

    Initial Modal styles

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import styles from './Modal.module.css';
 
 function Modal(props) {
   return(
-    <div>
-      <div>
+    <div className={styles.modalOuter}>
+      <div className={styles.modalInner}>
         {props.children}
       </div>
     </div>
