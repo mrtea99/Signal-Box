44ff48bb1e93c0451352a177f619b481042d3e06
Stephen Train <steve.train@gmail.com>
2020-10-03 11:48:49 +0100

    Date formatting for session list

--- src/components/SessionList/SessionList.js
+++ src/components/SessionList/SessionList.js
@@ -6,6 +6,37 @@ import Timer from '../Timer/Timer.js';
 import styles from './SessionList.module.css';
 
 function SessionList(props) {
+
+  function addLeadingZero(number) {
+    if (number < 10) {
+      number = '0' + number;
+    }
+
+    return number;
+  }
+
+  function formatDate(time) {
+    const dateObj = new Date(time);
+
+    const year = dateObj.getFullYear();
+    const month = addLeadingZero(dateObj.getMonth()+1);
+    const day = addLeadingZero(dateObj.getDate());
+
+    const dateString = `${year}-${month}-${day}`;
+
+    return dateString;
+  }
+
+  function formatTime(time) {
+    const dateObj = new Date(time);
+
+    const hour = addLeadingZero(dateObj.getHours());
+    const min = addLeadingZero(dateObj.getMinutes());
+    const sec = addLeadingZero(dateObj.getSeconds());
+
+    return `${hour}:${min}:${sec}`;
+  }
+
   return (
     <table className={styles.container}>
       <thead className={styles.header}>
@@ -21,8 +52,16 @@ function SessionList(props) {
       {props.thisRunData['stages'][props.thisStage].map((session, index) =>
         <tr key={session.sessionUid} className={styles.itemRow}>
           <td className={styles.contentItem}>{session.activity}</td>
-          <td className={styles.contentItem}>{new Date(session.startTime).toISOString()}</td>
-          <td className={styles.contentItem}>{session.endTime ? new Date(session.endTime).toISOString() : ''}</td>
+          <td className={styles.contentItem}>
+            <time dateTime={new Date(session.startTime).toISOString()}>{formatDate(session.startTime)}<br />{formatTime(session.startTime)}</time>
+          </td>
+          <td className={styles.contentItem}>
+            {session.endTime ?
+              <time dateTime={new Date(session.endTime).toISOString()}>{formatDate(session.endTime)}<br />{formatTime(session.endTime)}</time>
+            :
+              <></>
+            }
+          </td>
           <td className={styles.contentItem}>
             {session.endTime ? 
               <TimeFormater rawTime={new Date(session.endTime) - new Date(session.startTime)} />
