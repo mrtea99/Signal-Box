3088d7ae752fcc1834173f0da9d72875d05bd9a7
Stephen Train <mrtea99@gmail.com>
2020-10-12 19:12:31 +0100

    Switching modals touse react portals

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,11 +1,21 @@
 import React from "react";
+import { createPortal } from "react-dom";
 import styles from "./Modal.module.css";
 
 function Modal(props) {
-  return (
+  const mount = document.getElementById("modal-root");
+  const el = document.createElement("div");
+
+  React.useEffect(() => {
+    mount.appendChild(el);
+    return () => mount.removeChild(el);
+  }, [el, mount]);
+
+  return createPortal(
     <div className={styles.modalOuter}>
       <div className={styles.modalInner}>{props.children}</div>
-    </div>
+    </div>,
+    el
   );
 }
 
