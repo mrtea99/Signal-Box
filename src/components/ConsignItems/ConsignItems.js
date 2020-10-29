c8470f5e66b209018d3c16055388117b94904fe9
Stephen Train <mrtea99@gmail.com>
2020-10-29 12:27:10 +0000

    Displaying activity totals in consignment modal

--- src/components/ConsignItems/ConsignItems.js
+++ src/components/ConsignItems/ConsignItems.js
@@ -37,8 +37,62 @@ function ConsignItems(props) {
     props.updateRunData(props.thisRunData.uid, null, updateBadField, countBad);
   };
 
+  const buildTotals = function () {
+    const allSessions = props.thisRunData.stages[props.thisStage]["sessions"];
+
+    let totalsData = {};
+
+    allSessions.forEach((session, index) => {
+      // const activityDataNew = {
+      //   amount: session.amount,
+      //   amountBad: session.amountBad,
+      // };
+
+      const activityKey = session.type === "qa" ? "QA" : session.activity;
+
+      const activityDataOld = totalsData[activityKey];
+
+      if (activityDataOld) {
+        activityDataOld.amount += session.amount ? session.amount : 0;
+        activityDataOld.amountBad += session.amountBad ? session.amountBad : 0;
+      } else {
+        totalsData[activityKey] = {
+          amount: session.amount,
+          amountBad: session.amountBad,
+        };
+      }
+    });
+
+    console.log(totalsData);
+    return totalsData;
+  };
+
+  const tots = buildTotals();
+
   return (
     <ModalControl triggerCopy="Consign" handleSubmit={handleSubmit}>
+      <div>
+        <table>
+          <thead>
+            <tr>
+              <th>Activity</th>
+              <th>Items</th>
+              <th>Defective</th>
+            </tr>
+          </thead>
+          <tbody>
+            {Object.keys(tots).map((key) => {
+              return (
+                <tr key={key}>
+                  <td>{key}</td>
+                  <td>{tots[key].amount}</td>
+                  <td>{tots[key].amountBad}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
       <div>
         <label htmlFor="consign-items">Consignment amount</label>
         <input
