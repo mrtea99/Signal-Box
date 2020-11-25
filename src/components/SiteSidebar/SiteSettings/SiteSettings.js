c09db04d96059c40271550b0ae6858b90f0016e5
Stephen Train <mrtea99@gmail.com>
2020-11-25 18:05:57 +0000

    Adding all site settings toggles

--- src/components/SiteSidebar/SiteSettings/SiteSettings.js
+++ src/components/SiteSidebar/SiteSettings/SiteSettings.js
@@ -3,22 +3,16 @@ import FormItem from "../../FormItem/FormItem";
 
 function SiteSettings() {
   const [lang, setLang] = React.useState("en-us");
+  const [unitSystem, setUnitSystem] = React.useState("metric");
+  const [timeFormat, setTimeFormat] = React.useState("24h");
+  const [siteTheme, setSiteTheme] = React.useState("dark");
+  const [viewMode, setViewMode] = React.useState("full");
 
   return (
     <form>
       {/* Language */}
-      {lang}
       <FormItem
         type="toggleButton"
-        label="Language:"
-        ident="site-lang2"
-        itemLabels={["English", "Español"]}
-        itemValues={["en-us", "es-mx"]}
-        value={lang}
-        updateHandler={setLang}
-      />
-      <FormItem
-        type="radioGroup"
         label="Language:"
         ident="site-lang"
         itemLabels={["English", "Español"]}
@@ -27,9 +21,45 @@ function SiteSettings() {
         updateHandler={setLang}
       />
       {/* Units */}
+      <FormItem
+        type="toggleButton"
+        label="Units:"
+        ident="site-unit-system"
+        itemLabels={["Metric", "U.S. Customary"]}
+        itemValues={["metric", "us"]}
+        value={unitSystem}
+        updateHandler={setUnitSystem}
+      />
       {/* Time Format */}
+      <FormItem
+        type="toggleButton"
+        label="Time Format:"
+        ident="site-time-format"
+        itemLabels={["24h", "12h"]}
+        itemValues={["24h", "12h"]}
+        value={timeFormat}
+        updateHandler={setTimeFormat}
+      />
       {/* Theme */}
+      <FormItem
+        type="toggleButton"
+        label="Theme:"
+        ident="site-theme"
+        itemLabels={["Dark", "Light"]}
+        itemValues={["dark", "light"]}
+        value={siteTheme}
+        updateHandler={setSiteTheme}
+      />
       {/* Simple mode */}
+      <FormItem
+        type="toggleButton"
+        label="View Mode:"
+        ident="site-view-mode"
+        itemLabels={["Full", "Simple"]}
+        itemValues={["full", "simple"]}
+        value={viewMode}
+        updateHandler={setViewMode}
+      />
     </form>
   );
 }
