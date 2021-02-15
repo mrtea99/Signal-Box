b06ea9ec19afa61db4f90c068c5e2e9cc94a3dea
Stephen Train <mrtea99@gmail.com>
2021-02-15 12:31:47 +0000

    Assing assignee and notes fields for opning assignment

--- src/components/AssignmentOpener/AssignmentOpener.js
+++ src/components/AssignmentOpener/AssignmentOpener.js
@@ -1,16 +1,60 @@
 import React from "react";
 
 import ModalControl from "../Modal/ModalControl/ModalControl.js";
+import FormItem from "../FormItem/FormItem.js";
+import UserSelect from "../FormItem/UserSelect/UserSelect.js";
 
-function AssignmentOpener() {
+function AssignmentOpener(props) {
+  const [description, setDescription] = React.useState("");
+  const [assignee, setAssignee] = React.useState(null);
+
+  const handleSubmit = function () {
+    const newsessionId = Date.now();
+
+    const newSession = {
+      sessionId: newsessionId,
+      type: "assign",
+      startTime: Date.now(),
+      endTime: null,
+      user: props.activeUser,
+      secondaryUser: assignee,
+      notes: description,
+    };
+
+    props.addSession(newSession, props.thisStage);
+
+    handleCancel();
+  };
+
+  const handleCancel = function () {
+    setDescription("");
+  };
+
   return (
     <ModalControl
       title="Assign Stage"
-      // handleSubmit={}
-      // handleCancel={}
+      handleSubmit={handleSubmit}
+      handleCancel={handleCancel}
       triggerCopy={"Assign Stage"}
       buttonAttrs={{ fillWidth: true, color: "assign", icon: "assign" }}
-    ></ModalControl>
+    >
+      <form>
+        <UserSelect
+          label="Assignee:"
+          ident={"assignee-" + props.thisStage}
+          updateHandler={(value) => setAssignee(value)}
+          value={assignee}
+        />
+        <FormItem
+          label="Notes:"
+          type="textarea"
+          ident="assignment-notes"
+          updateHandler={(value) => {
+            setDescription(value);
+          }}
+        />
+      </form>
+    </ModalControl>
   );
 }
 
