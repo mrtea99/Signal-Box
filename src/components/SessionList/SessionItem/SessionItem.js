9b83fe0d3ad683bb1de490912db8ac571ecc3027
Stephen Train <mrtea99@gmail.com>
2021-03-01 11:17:59 +0000

    Refactoring session list items into new component

---
+++ src/components/SessionList/SessionItem/SessionItem.js
@@ -0,0 +1,235 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+import DateTimeFormatter from "../../DateTimeFormatter/DateTimeFormatter.js";
+import SessionDuration from "../SessionDuration/SessionDuration.js";
+import FlagCloser from "../../FlagCloser/FlagCloser.js";
+import AssignmentCloser from "../../AssignmentCloser/AssignmentCloser.js";
+import CheckCloser from "../../CheckCloser/CheckCloser.js";
+import ModalControl from "../../Modal/ModalControl/ModalControl.js";
+import SessionDetails from "../SessionDetails/SessionDetails.js";
+import DurationFormatter from "../../DurationFormatter/DurationFormatter.js";
+import Repeater from "../Repeater/Repeater.js";
+
+import getFlagName from "../../../utils/getFlagName.js";
+import getSessionName from "../../../utils/getSessionName.js";
+import getItemType from "../../../utils/getItemType.js";
+
+import styles from "./SessionItem.module.css";
+
+function SessionItem(props) {
+  const { session, itemCount, overview, thisStageData, thisStage, columns } = props;
+
+  const itemName = getItemType(thisStage);
+
+  let allResolved = true;
+  for (let i = 0; i < thisStageData.length; i++) {
+    if (!thisStageData[i].endTime) {
+      allResolved = false;
+    }
+  }
+
+  const findTotalDuration = function () {
+    let totalDuration = 0;
+
+    for (let i = 0; i < thisStageData.length; i++) {
+      if (thisStageData[i].type !== "assign") {
+        if (thisStageData[i].endTime) {
+          totalDuration =
+            totalDuration +
+            (thisStageData[i].endTime - thisStageData[i].startTime);
+        } else {
+          totalDuration =
+            totalDuration + (Date.now() - thisStageData[i].startTime);
+        }
+      }
+    }
+
+    return <DurationFormatter rawTime={totalDuration} ticking={!allResolved} />;
+  };
+
+  return (
+    <>
+      {!overview ? (
+        <ul
+          className={`${styles.itemRow} ${
+            styles[
+              "itemRow--" +
+                (session.type === "flag"
+                  ? getFlagName(session.amount, false)
+                  : session.type)
+            ]
+          } ${
+            session.endTime ? styles.itemRowResolved : styles.itemRowUnresolved
+          }`}
+          key={session.sessionId}
+        >
+          {/* count ---------- */}
+          <li
+            className={`${styles.contentItem} ${styles.colNumber} ${styles.colFixed} ${columns[0].className}`}
+          >
+            {itemCount}
+          </li>
+          {/* name ---------- */}
+          <li className={`${styles.contentItem} ${styles.colActivity} ${columns[1].className}`}>
+            {getSessionName(session)}
+          </li>
+          {/* start time ---------- */}
+          <li
+            className={`${styles.contentItem} ${styles.colStartTime} ${columns[2].className}`}
+          >
+            <span className={styles.cellLabel}>Start Time:</span>
+            <span className={styles.cellContent}>
+              <DateTimeFormatter date={session.startTime} />
+            </span>
+          </li>
+          {/* duration ---------- */}
+          <li
+            className={`${styles.contentItem} ${styles.colDuration} ${columns[3].className} `}
+          >
+            <span className={styles.cellLabel}>Duration:</span>
+            <span className={styles.cellContent}>
+              <SessionDuration session={session} />
+            </span>
+          </li>
+          {/* items ---------- */}
+          <li
+            className={`${styles.contentItem} ${styles.colItemsGood} ${columns[4].className}`}
+          >
+            <span className={styles.cellLabel}>{itemName}:</span>
+            <span className={styles.cellContent}>
+              {session.amount === undefined ||
+              session.amount === null ||
+              session.type === "flag"
+                ? "-"
+                : session.amount}
+            </span>
+          </li>
+          {/* defective items ---------- */}
+          <li
+            className={`${styles.contentItem} ${styles.colItemsBad} ${columns[5].className}`}
+          >
+            <span className={styles.cellLabel}>Defective:</span>
+            <span className={styles.cellContent}>
+              {session.amountBad === undefined || session.amount === null
+                ? "-"
+                : session.amountBad}
+            </span>
+          </li>
+          {/* users ---------- */}
+          <li
+            className={`${styles.contentItem} ${styles.colTech} ${columns[6].className}`}
+          >
+            <span className={styles.cellLabel}>Users:</span>
+            <span className={styles.cellContent}>
+              {/* {session.user} */}
+              {/* todo get actual name */}
+              {session.user === "1" ? "Jesus Sandoval" : "Amanda Kezios"}
+              {session.secondaryUser
+                ? " / " +
+                  (session.secondaryUser === "1"
+                    ? "Jesus Sandoval"
+                    : "Amanda Kezios")
+                : null}
+            </span>
+          </li>
+          {/* action ---------- */}
+          <li
+            className={`${styles.contentItem} ${styles.colAction} ${columns[7].className}`}
+          >
+            {session.type === "flag" ? (
+              <FlagCloser
+                key={session.sessionId}
+                thisStage={thisStage}
+                session={session}
+                endSession={props.endSession}
+                updateSession={props.updateSession}
+                activeUser={props.activeUser}
+              />
+            ) : null}
+            {session.type === "assign" ? (
+              <AssignmentCloser
+                key={session.sessionId}
+                thisStage={thisStage}
+                session={session}
+                updateSession={props.updateSession}
+                endSession={props.endSession}
+              />
+            ) : null}
+            {session.type === "qa" ? (
+              <CheckCloser
+                key={session.sessionId}
+                thisStage={thisStage}
+                session={session}
+                endSession={props.endSession}
+              />
+            ) : null}
+          </li>
+          {/* details ---------- */}
+          <li
+            className={`${styles.contentItem} ${styles.colInfo} ${columns[8].className}`}
+          >
+            <ModalControl
+              title="Session Details"
+              triggerCopy={""}
+              buttonAttrs={{
+                icon:
+                  session.notes && session.notes.length
+                    ? "detailsAlt"
+                    : "details",
+              }}
+            >
+              <SessionDetails session={session} thisStage={thisStage} />
+            </ModalControl>
+          </li>
+        </ul>
+      ) : (
+        <ul className={`${styles.itemRow} ${styles.itemRowTotals}`}>
+          <li className={`${styles.contentItem} ${styles.colNumber} ${columns[0].className}`}></li>
+          <li className={`${styles.contentItem} ${styles.colActivity} ${columns[1].className}`}>
+            Overall
+          </li>
+          <li className={`${styles.contentItem} ${styles.colStartTime} ${columns[2].className}`}>
+            <span className={styles.cellLabel}>Start Time:</span>
+            <span className={styles.cellContent}>
+              {thisStageData.length ? (
+                <DateTimeFormatter
+                  date={thisStageData[0].startTime}
+                  splitLines={true}
+                />
+              ) : (
+                "-"
+              )}
+            </span>
+          </li>
+          <li className={`${styles.contentItem} ${styles.colDuration} ${columns[3].className}`}>
+            <span className={styles.cellLabel}>Duration:</span>
+            <span className={styles.cellContent}>
+              {allResolved ? (
+                findTotalDuration()
+              ) : (
+                <Repeater interval={333} callback={findTotalDuration} />
+              )}
+            </span>
+          </li>
+          <li className={`${styles.contentItem} ${styles.colItemsGood} ${columns[4].className}`}></li>
+          <li className={`${styles.contentItem} ${styles.colItemsBad} ${columns[5].className}`}></li>
+          <li className={`${styles.contentItem} ${styles.colTech} ${columns[6].className}`}></li>
+          <li className={`${styles.contentItem} ${styles.colAction} ${columns[7].className}`}></li>
+          <li className={`${styles.contentItem} ${styles.colInfo} ${columns[8].className}`}></li>
+        </ul>
+      )}
+    </>
+  );
+}
+
+SessionItem.propTypes = {
+  session: PropTypes.object,
+  itemCount: PropTypes.number,
+  thisStage: PropTypes.number,
+  endSession: PropTypes.func,
+  updateSession: PropTypes.func,
+  activeUser: PropTypes.string,
+};
+
+export default SessionItem;
--- src/components/SessionList/SessionList.js
+++ src/components/SessionList/SessionList.js
@@ -1,54 +1,18 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-import DurationFormatter from "../DurationFormatter/DurationFormatter.js";
-import FlagCloser from "../FlagCloser/FlagCloser.js";
-import CheckCloser from "../CheckCloser/CheckCloser.js";
-import ModalControl from "../Modal/ModalControl/ModalControl.js";
-import Repeater from "./Repeater/Repeater.js";
 import TableHeader from "../TableHeader/TableHeader.js";
-import DateTimeFormatter from "../DateTimeFormatter/DateTimeFormatter.js";
-import AssignmentCloser from "../AssignmentCloser/AssignmentCloser.js";
-import SessionDetails from "./SessionDetails/SessionDetails.js";
 
 import getItemType from "../../utils/getItemType.js";
-import getSessionName from "../../utils/getSessionName.js";
-import getFlagName from "../../utils/getFlagName.js";
 
 import styles from "./SessionList.module.css";
-import SessionDuration from "./SessionDuration/SessionDuration.js";
+import SessionItem from "./SessionItem/SessionItem.js";
 
 function SessionList(props) {
   const thisStageData =
     props.thisRunData["stages"][props.thisStage]["sessions"];
 
   const itemName = getItemType(props.thisStage);
-
-  let allResolved = true;
-  for (let i = 0; i < thisStageData.length; i++) {
-    if (!thisStageData[i].endTime) {
-      allResolved = false;
-    }
-  }
-
-  const findTotalDuration = function () {
-    let totalDuration = 0;
-
-    for (let i = 0; i < thisStageData.length; i++) {
-      if (thisStageData[i].type !== "assign") {
-        if (thisStageData[i].endTime) {
-          totalDuration =
-            totalDuration +
-            (thisStageData[i].endTime - thisStageData[i].startTime);
-        } else {
-          totalDuration =
-            totalDuration + (Date.now() - thisStageData[i].startTime);
-        }
-      }
-    }
-
-    return <DurationFormatter rawTime={totalDuration} />;
-  };
 
   const columns = [
     { copy: "№", className: `${styles.colNumber} ${styles.colFixed}` },
@@ -82,171 +46,24 @@ function SessionList(props) {
               className={styles.sessionListItem}
               key={"sli-" + session.sessionId}
             >
-              <ul
-                className={`${styles.itemRow} ${
-                  styles[
-                    "itemRow--" +
-                      (session.type === "flag"
-                        ? getFlagName(session.amount, false)
-                        : session.type)
-                  ]
-                } ${
-                  session.endTime
-                    ? styles.itemRowResolved
-                    : styles.itemRowUnresolved
-                }`}
-                key={session.sessionId}
-              >
-                <li
-                  className={`${styles.contentItem} ${styles.colNumber} ${styles.colFixed}`}
-                >
-                  {thisStageData.length - index}
-                </li>
-                <li className={`${styles.contentItem} ${styles.colActivity}`}>
-                  {getSessionName(session)}
-                </li>
-                <li
-                  className={`${styles.contentItem} ${styles.colStartTime} ${styles.colFixed}`}
-                >
-                  <span className={styles.cellLabel}>Start Time:</span>
-                  <span className={styles.cellContent}>
-                    <DateTimeFormatter date={session.startTime} />
-                  </span>
-                </li>
-                <li
-                  className={`${styles.contentItem} ${styles.colDuration} ${styles.colFixed}`}
-                >
-                  <span className={styles.cellLabel}>Duration:</span>
-                  <span className={styles.cellContent}>
-                    <SessionDuration session={session} />
-                  </span>
-                </li>
-                <li
-                  className={`${styles.contentItem} ${styles.colItemsGood} ${styles.colFixed}`}
-                >
-                  <span className={styles.cellLabel}>{itemName}:</span>
-                  <span className={styles.cellContent}>
-                    {session.amount === undefined ||
-                    session.amount === null ||
-                    session.type === "flag"
-                      ? "-"
-                      : session.amount}
-                  </span>
-                </li>
-                <li
-                  className={`${styles.contentItem} ${styles.colItemsBad} ${styles.colFixed}`}
-                >
-                  <span className={styles.cellLabel}>Defective:</span>
-                  <span className={styles.cellContent}>
-                    {session.amountBad === undefined || session.amount === null
-                      ? "-"
-                      : session.amountBad}
-                  </span>
-                </li>
-                <li
-                  className={`${styles.contentItem} ${styles.colTech} ${styles.colFixed}`}
-                >
-                  <span className={styles.cellLabel}>Users:</span>
-                  <span className={styles.cellContent}>
-                    {/* {session.user} */}
-                    {/* todo get actual name */}
-                    {session.user === "1" ? "Jesus Sandoval" : "Amanda Kezios"}
-                    {session.secondaryUser
-                      ? " / " +
-                        (session.secondaryUser === "1"
-                          ? "Jesus Sandoval"
-                          : "Amanda Kezios")
-                      : null}
-                  </span>
-                </li>
-                <li
-                  className={`${styles.contentItem} ${styles.colAction} ${styles.colFixed}`}
-                >
-                  {session.type === "flag" ? (
-                    <FlagCloser
-                      key={session.sessionId}
-                      thisStage={props.thisStage}
-                      session={session}
-                      endSession={props.endSession}
-                      updateSession={props.updateSession}
-                      activeUser={props.activeUser}
-                    />
-                  ) : null}
-                  {session.type === "assign" ? (
-                    <AssignmentCloser
-                      key={session.sessionId}
-                      thisStage={props.thisStage}
-                      session={session}
-                      updateSession={props.updateSession}
-                      endSession={props.endSession}
-                    />
-                  ) : null}
-                  {session.type === "qa" ? (
-                    <CheckCloser
-                      key={session.sessionId}
-                      thisStage={props.thisStage}
-                      session={session}
-                      endSession={props.endSession}
-                    />
-                  ) : null}
-                </li>
-                <li
-                  className={`${styles.contentItem} ${styles.colInfo} ${styles.colFixed}`}
-                >
-                  <ModalControl
-                    title="Session Details"
-                    triggerCopy={""}
-                    buttonAttrs={{
-                      icon:
-                        session.notes && session.notes.length
-                          ? "detailsAlt"
-                          : "details",
-                    }}
-                  >
-                    <SessionDetails
-                      session={session}
-                      thisStage={props.thisStage}
-                    />
-                  </ModalControl>
-                </li>
-              </ul>
+              <SessionItem
+                session={session}
+                thisStageData={thisStageData}
+                itemCount={thisStageData.length - index}
+                thisStage={props.thisStage}
+                endSession={props.endSession}
+                updateSession={props.updateSession}
+                activeUser={props.activeUser}
+                columns={columns}
+              />
             </li>
           ))}
 
-        <li className={`${styles.itemRow} ${styles.itemRowTotals}`}>
-          <div className={`${styles.contentItem} ${styles.colNumber}`}></div>
-          <div className={`${styles.contentItem} ${styles.colActivity}`}>
-            Overall
-          </div>
-          <div className={`${styles.contentItem} ${styles.colStartTime}`}>
-            <span className={styles.cellLabel}>Start Time:</span>
-            <span className={styles.cellContent}>
-              {thisStageData.length ? (
-                <DateTimeFormatter
-                  date={thisStageData[0].startTime}
-                  splitLines={true}
-                />
-              ) : (
-                "-"
-              )}
-            </span>
-          </div>
-          <div className={`${styles.contentItem} ${styles.colDuration}`}>
-            <span className={styles.cellLabel}>Duration:</span>
-            <span className={styles.cellContent}>
-              {allResolved ? (
-                findTotalDuration()
-              ) : (
-                <Repeater interval={333} callback={findTotalDuration} />
-              )}
-            </span>
-          </div>
-          <div className={`${styles.contentItem} ${styles.colItemsGood}`}></div>
-          <div className={`${styles.contentItem} ${styles.colItemsBad}`}></div>
-          <div className={`${styles.contentItem} ${styles.colTech}`}></div>
-          <div className={`${styles.contentItem} ${styles.colAction}`}></div>
-          <div className={`${styles.contentItem} ${styles.colInfo}`}></div>
-        </li>
+        <SessionItem
+          overview={true}
+          thisStageData={thisStageData}
+          columns={columns}
+        />
       </ul>
     </div>
   );
