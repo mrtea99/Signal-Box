dcf89f82e61709cc1d5bf320ea6150175725ff12
Stephen Train <mrtea99@gmail.com>
2021-04-06 10:50:22 +0100

    Fixing sidebar logo path

--- src/components/SiteSidebar/SiteSidebar.js
+++ src/components/SiteSidebar/SiteSidebar.js
@@ -21,7 +21,7 @@ function SiteSidebar(props) {
       <div className={styles.sidebarControl}>
         <Button onClick={closeSidebar} icon="cross" color="cancel" />
       </div>
-      <img className={styles.siteLogo} alt="Mojo Spa Logo" src="./logo.svg" />
+      <img className={styles.siteLogo} alt="Mojo Spa Logo" src="/logo.svg" />
       {props.children}
     </div>
   );
