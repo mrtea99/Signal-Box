461e6d02d8a08b5d90e56f9602af141ea0d1764a
Stephen Train <mrtea99@gmail.com>
2021-04-30 10:39:51 +0100

    Rounding timer up to nearest minute

--- src/components/DurationFormatter/DurationFormatter.js
+++ src/components/DurationFormatter/DurationFormatter.js
@@ -9,6 +9,9 @@ import styles from "./DurationFormatter.module.css";
  */
 
 function DurationFormatter(props) {
+  //todo add site setting to toggle seconds display
+  const showSeconds = false;
+
   let duraton = props.rawTime;
 
   const negativeTime = duraton < 0;
@@ -16,21 +19,35 @@ function DurationFormatter(props) {
     duraton = Math.abs(duraton);
   }
 
+  //Add one minute to duration to stop timer being 00:00 for first minute
+  if (!showSeconds) {
+    duraton = duraton + 60000;
+  }
+
   let hours = Math.floor(duraton / (1000 * 60 * 60));
   let minutes = Math.floor((duraton / (1000 * 60)) % 60);
-  // let seconds = Math.floor((duraton / 1000) % 60);
+  let seconds = Math.floor((duraton / 1000) % 60);
 
   hours = hours < 10 ? "0" + hours : hours;
   minutes = minutes < 10 ? "0" + minutes : minutes;
-  // seconds = seconds < 10 ? "0" + seconds : seconds;
+  seconds = seconds < 10 ? "0" + seconds : seconds;
+
+  const tickingSeparator = (
+    <span className={props.ticking ? styles.separatorTicking : ""}>:</span>
+  );
 
   return (
     <span className="duration">
       {negativeTime ? "-" : null}
       {hours}
-      <span className={props.ticking ? styles.separatorTicking : ""}>:</span>
+      {tickingSeparator}
       {minutes}
-      {/* :{seconds} */}
+      {showSeconds ? (
+        <>
+          {tickingSeparator}
+          {seconds}
+        </>
+      ) : null}
     </span>
   );
 }
